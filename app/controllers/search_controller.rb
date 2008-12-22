2e0cbf786e3e2c350123ecd23fc4792c73837e04
Jim Balhoff <balhoff@nescent.org>
2008-12-22 21:47:05 +0000

    Fix for server name.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,18 +1,20 @@
 class SearchController < ApplicationController
   
+  SERVER = "phenoscape-dev.nescent.org"
+  
   def index
   end
   
   def anatomy
     @term = params[:id]
-    response = Net::HTTP.get_response("localhost", "/OBD-WS/phenotypes/summary/anatomy/" + @term)
+    response = Net::HTTP.get_response(SERVER, "/OBD-WS/phenotypes/summary/anatomy/" + @term)
     #response = Net::HTTP.get_response("localhost", "/javascripts/dummy_anatomy_summary.js")
     @summary = ActiveSupport::JSON.decode(response.body)
   end
   
   def gene
     @term = params[:id]
-    response = Net::HTTP.get_response("localhost", "/javascripts/dummy_gene_summary.js")
+    response = Net::HTTP.get_response(SERVER, "/javascripts/dummy_gene_summary.js")
     @results = ActiveSupport::JSON.decode(response.body)
   end
   
@@ -24,12 +26,12 @@ class SearchController < ApplicationController
     type = params[:type]
     if type == "taxa"
       #response = Net::HTTP.get_response("localhost", "/OBD-WS/phenotypes/anatomy/" + params[:id] + "/taxa/" + params[:quality])
-      response = Net::HTTP.get_response("localhost", "/javascripts/dummy_anatomy_results_taxon.js")
+      response = Net::HTTP.get_response(SERVER, "/javascripts/dummy_anatomy_results_taxon.js")
       @results = ActiveSupport::JSON.decode(response.body)
       render(:action => "taxa_phenotypes")
     else
       #response = Net::HTTP.get_response("localhost", "/OBD-WS/phenotypes/anatomy/" + params[:id] + "/genes/" + params[:quality])
-      response = Net::HTTP.get_response("localhost", "/javascripts/dummy_anatomy_results_gene.js")
+      response = Net::HTTP.get_response(SERVER, "/javascripts/dummy_anatomy_results_gene.js")
       @results = ActiveSupport::JSON.decode(response.body)
       render(:action => "genes_phenotypes")
     end
