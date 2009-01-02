24a424e7eeb7f264f3d37859e4bc81871d61d8c1
Jim Balhoff <balhoff@nescent.org>
2009-01-02 19:21:59 +0000

    Fixed hostname bug in results pages.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -24,12 +24,12 @@ class SearchController < ApplicationController
   def phenotypes
     type = params[:type]
     if type == "taxa"
-      response = Net::HTTP.get_response("localhost", "/OBD-WS/phenotypes/anatomy/" + params[:id] + "/taxa/" + params[:quality])
+      response = Net::HTTP.get_response(self.request.host, "/OBD-WS/phenotypes/anatomy/" + params[:id] + "/taxa/" + params[:quality])
       #response = Net::HTTP.get_response(self.request.host, "/javascripts/dummy_anatomy_results_taxon.js")
       @results = ActiveSupport::JSON.decode(response.body)
       render(:action => "taxa_phenotypes")
     else
-      response = Net::HTTP.get_response("localhost", "/OBD-WS/phenotypes/anatomy/" + params[:id] + "/genes/" + params[:quality])
+      response = Net::HTTP.get_response(self.request.host, "/OBD-WS/phenotypes/anatomy/" + params[:id] + "/genes/" + params[:quality])
       #response = Net::HTTP.get_response(self.request.host, "/javascripts/dummy_anatomy_results_gene.js")
       @results = ActiveSupport::JSON.decode(response.body)
       render(:action => "genes_phenotypes")
