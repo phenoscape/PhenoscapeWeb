020e0d2b809c87baa6a8037cf4141b5bce17037e
Jim Balhoff <balhoff@nescent.org>
2009-02-19 15:44:56 +0000

    Mockup files.

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -5,6 +5,10 @@ class SearchController < ApplicationController
   
   def anatomy
     @term = params[:id]
+    ##mockup##
+    render(:action => "anatomy_mockup")
+    return
+    ##mockup##
     begin
       response = Net::HTTP.get_response(self.request.host, "/OBD-WS/phenotypes/summary/anatomy/" + @term)
     #response = Net::HTTP.get_response("localhost", "/javascripts/dummy_anatomy_summary.js")
@@ -18,6 +22,10 @@ class SearchController < ApplicationController
   
   def gene
     @term = params[:id]
+    ##mockup##
+    render(:action => "gene_mockup")
+    return
+    ##mockup##
     begin
     #response = Net::HTTP.get_response(self.request.host, "/javascripts/dummy_gene_summary.js")
       response = Net::HTTP.get_response(self.request.host, "/OBD-WS/phenotypes/summary/gene/" + @term)
@@ -29,12 +37,20 @@ class SearchController < ApplicationController
   
   def taxonomy
     @term = params[:id]
+    ##mockup##
+    render(:action => "taxon_mockup")
+    return
+    ##mockup##
   end
   
   def phenotypes
     type = params[:type]
     begin
       if type == "taxa"
+        ##mockup##
+        render(:action => "taxa_phenotypes_mockup")
+        return
+        ##mockup##
         response = Net::HTTP.get_response(self.request.host, "/OBD-WS/phenotypes/anatomy/" + params[:id] + "/taxa/" + params[:quality])
         #response = Net::HTTP.get_response(self.request.host, "/javascripts/dummy_anatomy_results_taxon.js")
         @results = ActiveSupport::JSON.decode(response.body)
@@ -49,6 +65,20 @@ class SearchController < ApplicationController
     rescue Timeout::Error
       render(:action => "generic_timeout")
     end
+  end
+  
+  def annotation
+    ##mockup##
+    render(:action => "annotation_mockup")
+    return
+    ##mockup##
+  end
+  
+  def publication
+    ##mockup##
+    render(:action => "publication_mockup")
+    return
+    ##mockup##
   end
   
 end
--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -1,2 +1,222 @@
 module SearchHelper
+  
+  def anatomy_mockup_data
+    return {
+      "term" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+      "attributes" => [
+        {"id" => "PATO:12345", 
+          "name" => "shape", 
+          "values" => [ 
+              {"id" => "PATO:12345", "name" => "serrated"}, 
+              {"id" => "PATO:12345", "name" => "pointy"}, 
+              {"id" => "PATO:12345", "name" => "round"} 
+          ],
+          "taxon_annotations" => {"taxon_count" => 25, "annotation_count" => 43},
+          "gene_annotations" => {"gene_count" => 3, "annotation_count" => 5}
+        },
+        {"id" => "PATO:12345", 
+          "name" => "color", 
+          "values" => [ 
+              {"id" => "PATO:12345", "name" => "green"}, 
+              {"id" => "PATO:12345", "name" => "blue"}, 
+              {"id" => "PATO:12345", "name" => "yellow"} 
+          ],
+          "taxon_annotations" => {"taxon_count" => 101, "annotation_count" => 321},
+          "gene_annotations" => {"gene_count" => 7, "annotation_count" => 13}
+        },
+        {"id" => "PATO:12345", 
+          "name" => "texture", 
+          "values" => [ 
+              {"id" => "PATO:12345", "name" => "smooth"}, 
+              {"id" => "PATO:12345", "name" => "rough"}
+          ],
+          "taxon_annotations" => {"taxon_count" => 25, "annotation_count" => 43},
+          "gene_annotations" => {"gene_count" => 0, "annotation_count" => 0}
+        }
+      ]
+    }
+  end
+  
+  def gene_mockup_data
+    return {
+      "gene" => {"id" => "ZFIN:ZDB-GENE-990712-18", "name" => "eya1"},
+      "phenotypes" => [
+        {
+          "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+          "attribute" => {"id" => "PATO:12345", "name" => "shape"}, 
+          "values" => [ 
+              {"id" => "PATO:12345", "name" => "serrated"}, 
+              {"id" => "PATO:12345", "name" => "pointy"}, 
+              {"id" => "PATO:12345", "name" => "round"} 
+          ],
+          "related_genes" => [
+              {"id" => "ZFIN:ZDB-GENE-990712-18", "name" => "eya2"},
+              {"id" => "ZFIN:ZDB-GENE-990712-18", "name" => "eya3"}
+          ],
+          "related_taxa" => [
+              {"id" => "TTO:222222", "name" => "Ictalurus punctatus"},
+              {"id" => "TTO:222222", "name" => "Danio rerio"}
+          ]
+        },
+        {
+          "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+          "attribute" => {"id" => "PATO:12345", "name" => "shape"}, 
+          "values" => [ 
+              {"id" => "PATO:12345", "name" => "serrated"}, 
+              {"id" => "PATO:12345", "name" => "pointy"}, 
+              {"id" => "PATO:12345", "name" => "round"} 
+          ],
+          "related_genes" => [
+              {"id" => "ZFIN:ZDB-GENE-990712-18", "name" => "eya2"},
+              {"id" => "ZFIN:ZDB-GENE-990712-18", "name" => "eya3"}
+          ],
+          "related_taxa" => [
+              {"id" => "TTO:222222", "name" => "Ictalurus punctatus"},
+              {"id" => "TTO:222222", "name" => "Danio rerio"}
+          ]
+        },
+        {
+          "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+          "attribute" => {"id" => "PATO:12345", "name" => "shape"}, 
+          "values" => [ 
+              {"id" => "PATO:12345", "name" => "serrated"}, 
+              {"id" => "PATO:12345", "name" => "pointy"}, 
+              {"id" => "PATO:12345", "name" => "round"} 
+          ],
+          "related_genes" => [
+              {"id" => "ZFIN:ZDB-GENE-990712-18", "name" => "eya2"},
+              {"id" => "ZFIN:ZDB-GENE-990712-18", "name" => "eya3"}
+          ],
+          "related_taxa" => [
+              {"id" => "TTO:222222", "name" => "Ictalurus punctatus"},
+              {"id" => "TTO:222222", "name" => "Danio rerio"}
+          ]
+        }
+      ]
+    }
+  end
+  
+  def taxon_mockup_data
+    return {
+      "term" => {
+        "id" => "TTO:101020",
+        "name" => "Ictalurus",
+        "synonyms" => ["Elliops", "Haustor", "Istlarius", "Synechoglanis", "Villarius"]
+        },
+      "phenotypes" => [
+          {
+            "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+            "quality" => {"id" => "PATO:12345", "name" => "shape"},
+            "taxon_annotations" => {"taxon_count" => 25, "annotation_count" => 43},
+            "gene_annotations" => {"gene_count" => 4, "annotation_count" => 4}
+          },
+          {
+            "entity" => {"id" => "TAO:12345", "name" => "dorsal fin"},
+            "quality" => {"id" => "PATO:12345", "name" => "shape"},
+            "taxon_annotations" => {"taxon_count" => 25, "annotation_count" => 43},
+            "gene_annotations" => {"gene_count" => 0, "annotation_count" => 0}
+          },
+          {
+            "entity" => {"id" => "TAO:12345", "name" => "lateral line"},
+            "quality" => {"id" => "PATO:12345", "name" => "color"},
+            "taxon_annotations" => {"taxon_count" => 25, "annotation_count" => 43},
+            "gene_annotations" => {"gene_count" => 0, "annotation_count" => 0}
+          }
+        ]
+    }
+  end
+  
+  def taxon_phenotypes_mockup_data
+    return { #characters and all taxa["value-list"] must be the same length
+      "characters" => [
+          {
+            "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+            "quality" => {"id" => "PATO:12345", "name" => "shape"}
+          }
+        ],
+      "taxa" => [
+        {
+           "id" => "TTO:101020",
+            "name" => "Ictalurus punctatus",
+            "value-list" => [
+              [{"id" => "PATO:12345", "name" => "serrated"}]
+              ]
+        },
+        {
+           "id" => "TTO:101020",
+            "name" => "Ictalurus furcatus",
+            "value-list" => [
+              [{"id" => "PATO:12345", "name" => "serrated"}]
+              ]
+        },
+        {
+           "id" => "TTO:101020",
+            "name" => "Ictalurus lupus",
+            "value-list" => [
+              [{"id" => "PATO:12345", "name" => "serrated"}]
+              ]
+        }
+        ]
+    }
+  end
+  
+  def taxon_phenotypes_mockup_data_multiple
+    return { #characters and all taxa["value-list"] must be the same length
+      "included_taxa" => [{"id" => "TTO:101020", "name" => "Ictalurus"}],
+      "included_characters" => [
+        {
+          "entity" => {"id" => "TAO:12345", "name" => "bone"},
+          "quality" => {"id" => "PATO:12345", "name" => "shape"}
+        },
+        {
+          "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+          "quality" => {"id" => "PATO:12345", "name" => "color"}
+        }
+        ],
+      "characters" => [
+          {
+            "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+            "quality" => {"id" => "PATO:12345", "name" => "shape"}
+          },
+          {
+            "entity" => {"id" => "TAO:12345", "name" => "basihyal bone"},
+            "quality" => {"id" => "PATO:12345", "name" => "color"}
+          },
+          {
+            "entity" => {"id" => "TAO:12345", "name" => "ceratohyal bone"},
+            "quality" => {"id" => "PATO:12345", "name" => "shape"}
+          }
+        ],
+      "taxa" => [
+        {
+           "id" => "TTO:101020",
+            "name" => "Ictalurus punctatus",
+            "value-list" => [
+              [{"id" => "PATO:12345", "name" => "serrated"}],
+              [{"id" => "PATO:12345", "name" => "serrated"}],
+              [{"id" => "PATO:12345", "name" => "serrated"}]
+              ]
+        },
+        {
+           "id" => "TTO:101020",
+            "name" => "Ictalurus furcatus",
+            "value-list" => [
+              [{"id" => "PATO:12345", "name" => "yellow"}, {"id" => "PATO:12345", "name" => "green"}],
+              [{"id" => "PATO:12345", "name" => "yellow"}],
+              [{"id" => "PATO:12345", "name" => "blue"}]
+              ]
+        },
+        {
+           "id" => "TTO:101020",
+            "name" => "Ictalurus lupus",
+            "value-list" => [
+              [{"id" => "PATO:12345", "name" => "serrated"}],
+              [{"id" => "PATO:12345", "name" => "serrated"}],
+              [{"id" => "PATO:12345", "name" => "serrated"}]
+              ]
+        }
+        ]
+    }
+  end
+  
 end
