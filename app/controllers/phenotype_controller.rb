d3d596d43e185b68b7d4011019cea2ff6da5433e
Jim Balhoff <balhoff@nescent.org>
2009-04-28 19:23:51 +0000

    Updated gene summary page for new data format.

--- app/controllers/phenotype_controller.rb
+++ app/controllers/phenotype_controller.rb
@@ -1,25 +1,24 @@
 class PhenotypeController < ApplicationController
   
   def evo
-    formatAnnotationResults
+    formatAnnotationResults("evo")
   end
   
   
   def devo
-    formatAnnotationResults
-    #TODO fill in html template
+    formatAnnotationResults("devo")
   end
   
   
   private
-  def formatAnnotationResults
+  def formatAnnotationResults(type)
     #should check validity of entity and quality terms, and return 404? if one doesn't exist
      entity_response = Net::HTTP.get_response(self.request.host, "/OBD-WS/term/" + params[:entity])
      @entity = ActiveSupport::JSON.decode(entity_response.body)
      quality_response = Net::HTTP.get_response(self.request.host, "/OBD-WS/term/" + params[:quality])
      @quality = ActiveSupport::JSON.decode(quality_response.body)
      #response = Net::HTTP.get_response(self.request.host, "/javascripts/dummy_annotation_results.js")
-     url = "/OBD-WS/phenotypes?entity=" + params[:entity] + "&quality=" + params[:quality]
+     url = "/OBD-WS/phenotypes?entity=" + params[:entity] + "&quality=" + params[:quality] + "&type=" + type
      if params[:subject] != nil
        subject_response = Net::HTTP.get_response(self.request.host, "/OBD-WS/term/" + params[:subject])
        @subject = ActiveSupport::JSON.decode(subject_response.body)
