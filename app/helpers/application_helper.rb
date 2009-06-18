ec709d699bff6abab71bfc1bf2c828ffd7593399
Jim Balhoff <balhoff@nescent.org>
2009-06-18 18:27:48 +0000

    Revisions to anatomy page layout.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,6 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
   
-  
   def format_taxon(name)
     if name.include? " "
       return "<i>" + name + "</i>"
@@ -17,5 +16,26 @@ module ApplicationHelper
     return %Q'<a href="/search/taxon/#{id}" title="#{id}">#{name}</a>'
   end
   
+  @@subject_relation_mappings = {
+    "OBO_REL:is_a" => "is a type of",
+    "OBO_REL:part_of" => "is part of",
+    "part_of" => "is part of",
+    "OBO_REL:develops_from" => "develops from"
+  }
+  
+  @@object_relation_mappings = {
+    "OBO_REL:is_a" => "has subtype",
+    "OBO_REL:part_of" => "may have part",
+    "part_of" => "may have part",
+    "OBO_REL:develops_from" => "develops into"
+  }
+  
+  def subject_rel(id, default)
+    return (@@subject_relation_mappings.has_key? id) ? @@subject_relation_mappings[id] : default
+  end
+  
+  def object_rel(id, default)
+    return (@@object_relation_mappings.has_key? id) ? @@object_relation_mappings[id] : default
+  end
   
 end
