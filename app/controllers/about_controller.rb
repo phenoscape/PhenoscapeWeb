cbd5b0a127241ffe8a949935ec83910bcad32c4f
Jim Balhoff <balhoff@nescent.org>
2009-07-16 15:52:49 +0000

    Added "about relationships" page.

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,12 +1,19 @@
 ## FIXME missing documentation for class and methods.
 class AboutController < ApplicationController
   
+  caches_page :index
+  caches_page :services
+  caches_page :ontology_relationships
   
   def index
   end
   
   
   def services
+  end
+  
+  def ontology_relationships
+    @title = "Ontology Relationship Info"
   end
   
 end
