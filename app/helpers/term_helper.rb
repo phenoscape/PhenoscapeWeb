8a457e1df816e96baa2cf6cf3b93297c4ff12636
Jim Balhoff <balhoff@nescent.org>
2010-11-22 17:14:15 +0000

    Displaying synonym scope on entity term pages.

--- app/helpers/term_helper.rb
+++ app/helpers/term_helper.rb
@@ -1,5 +1,12 @@
 module TermHelper
   
+  SYNONYM_SCOPES = {"EXACT" => "exact", "RELATED" => "related"}
   
+  def formatted_synonym(synonym)
+    label = synonym["name"]
+    scope = SYNONYM_SCOPES[synonym["scope"]] if synonym.has_key? "scope"
+    label += %| <span class="synonym-scope">(#{scope})</span>|
+    return label
+  end
   
 end
