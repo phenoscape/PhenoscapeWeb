01d3c3f4de13f22d2c8d46cf318b195f6da55153
Edward Anderson <edward.anderson@scimedsolutions.com>
2011-06-08 15:34:34 -0400

    Save to workspace: Add/remove items from session when boxes are checked or unchecked
    Debug: log session contents on every workspace request

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,11 +1,36 @@
 class WorkspacesController < ActionController::Base
+
+  before_filter :initialize_session_workspace
+
   def show
     
   end
   
   def update
+    data = JSON.parse params['data']
+    type = data.keys.first
+    items = data.values
+    existing = session[:workspace][type] || []
+    session[:workspace][type] = (existing + items).uniq
   end
   
   def destroy
+    data = JSON.parse params['data']
+    type = data.keys.first
+    items = data.values
+    existing = session[:workspace][type] || []
+    existing.delete_if {|item| items.include? item}
+    session[:workspace][type] = existing
   end
+
+  after_filter :debug_session
+  private
+    def debug_session
+      logger.error "SESSION: #{session.inspect}"
+    end
+    
+    def initialize_session_workspace
+      session[:workspace] ||= {}
+    end
+  
 end
