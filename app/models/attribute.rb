40a6e977ec5addca2eae0825dca0f9176431b5b7
Edward Anderson <edward.anderson@scimedsolutions.com>
2011-10-14 20:25:58 -0400

    Variation tree: sort entities; default to shape

--- app/models/attribute.rb
+++ app/models/attribute.rb
@@ -2,7 +2,7 @@ class Attribute
   def self.all(options={})
     attributes_hash = Request.json_result('term/attributes')['attributes']
     if options[:format] == :array_for_select_options
-      return attributes_hash.map { |attr| [attr['name'], attr['id']] }
+      return attributes_hash.map { |attr| [attr['name'], attr['id']] }.sort_by { |name, id| name.downcase }
     end
     return attributes_hash
   end
